10564948bb90f7fcd0771c528da45415c212046e
hp <hp@LAPTOP-B7K6C05D>
2020-05-06 15:48:42 +0800

    RMI - Final Completion

--- src/Mails/Emails.java
+++ src/Mails/Emails.java
@@ -6,12 +6,15 @@ import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.*;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-public class GmailEmail {
+/**
+ *
+ * @author hp
+ */
+public class Emails {
     
     public static void main(String[] args) throws AddressException, MessagingException{
         
--- src/rmi/FireAlarmSensor.java
+++ src/rmi/FireAlarmSensor.java
@@ -1,15 +1,11 @@
 package rmi;
 
 import java.awt.Color;
-import java.awt.Component;
-import java.awt.Font;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import javax.swing.JOptionPane;
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 
 
